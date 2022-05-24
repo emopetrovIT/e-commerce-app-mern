725f5ce0d3bf2774b5d945bbdcf873ae21f2d4dc
emopetrovIT <em.petrov88@gmail.com>
2022-05-24 23:51:38 +0300

    Generate Sample Users

--- amazona/backend/data.js
+++ amazona/backend/data.js
@@ -1,4 +1,20 @@
+import bcrypt from 'bcryptjs';
+
 const data = {
+  users: [
+    {
+      name: 'Emilian',
+      email: 'admin@example.com',
+      password: bcrypt.hashSync('1234567'),
+      isAdmin: true,
+    },
+    {
+      name: 'John',
+      email: 'user@example.com',
+      password: bcrypt.hashSync('1234567'),
+      isAdmin: false,
+    },
+  ],
   products: [
     {
       // _id: '1',
