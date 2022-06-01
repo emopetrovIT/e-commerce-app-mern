5cffec2af6405e5a6980d2fdac53aaa643c84cf2
emopetrovIT <em.petrov88@gmail.com>
2022-06-01 22:54:36 +0300

    Implement Place Order Action

--- amazona/backend/server.js
+++ amazona/backend/server.js
@@ -1,9 +1,11 @@
 import express from 'express';
+// import data from './data.js';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import seedRouter from './routes/seedRoutes.js';
 import productRouter from './routes/productRoutes.js';
 import userRouter from './routes/userRoutes.js';
+import orderRouter from './routes/orderRoutes.js';
 
 dotenv.config();
 
@@ -24,6 +26,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/api/seed', seedRouter);
 app.use('/api/products', productRouter);
 app.use('/api/users', userRouter);
+app.use('/api/orders', orderRouter);
 
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
--- amazona/frontend/src/Store.js
+++ amazona/frontend/src/Store.js
@@ -51,6 +51,8 @@ function reducer(state, action) {
         cart: { ...state.cart, cartItems },
       };
     }
+    case 'CART_CLEAR':
+      return { ...state, cart: { ...state.cart, cartItems: [] } };
     case 'USER_SIGNIN':
       return { ...state, userInfo: action.payload };
     case 'USER_SIGNOUT':
